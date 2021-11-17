eed13b1746861c94b6f1add1b773b909eb00d08b
KODE LLC <vs@kode.ru>
2021-11-18 01:26:40 +0300

    Add toothpick dependency for auth:ui

--- auth/ui/src/main/java/ru/kode/base/internship/ui/DummyScreenPresenter.kt
+++ auth/ui/src/main/java/ru/kode/base/internship/ui/DummyScreenPresenter.kt
@@ -3,8 +3,9 @@ package ru.kode.base.internship.ui
 import ru.dimsuz.unicorn.coroutines.MachineDsl
 import ru.kode.base.core.BaseViewIntents
 import ru.kode.base.core.coroutine.BasePresenter
+import javax.inject.Inject
 
-internal class DummyScreenPresenter : BasePresenter<Unit, BaseViewIntents, Unit>() {
+internal class DummyScreenPresenter @Inject constructor() : BasePresenter<Unit, BaseViewIntents, Unit>() {
 
   override fun MachineDsl<Unit, Unit>.buildMachine() {
     initial = Unit to null
